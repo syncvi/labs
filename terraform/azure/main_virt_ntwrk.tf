03537032f48b74f8d18807abad1ef020558aa55f
syncvi <pkg417@proton.me>
2024-10-10 14:57:45 +0200

    tf locales

--- terraform/azure/main_virt_ntwrk.tf
+++ terraform/azure/main_virt_ntwrk.tf
@@ -15,16 +15,24 @@ provider "azurerm" {
   }
 }
 
+locals {
+  resource_group_name = "demo-grp"
+  location            = "Central India"
+  virt_ntwrk = {
+    name          = "example-network"
+    address_space = ["10.0.0.0/16"]
+  }
+}
 resource "azurerm_resource_group" "demogrp" {
-  name     = "demo-grp"
-  location = "Central India"
+  name     = local.resource_group_name
+  location = local.location
 }
 
 resource "azurerm_virtual_network" "demovirnet" {
-  name                = "example-network"
-  location            = azurerm_resource_group.demogrp.location
-  resource_group_name = azurerm_resource_group.demogrp.name
-  address_space       = ["10.0.0.0/16"]
+  name                = local.virt_ntwrk.name
+  location            = local.location
+  resource_group_name = local.resource_group_name
+  address_space       = local.virt_ntwrk.address_space
 
   subnet {
     name             = "subnetA"
