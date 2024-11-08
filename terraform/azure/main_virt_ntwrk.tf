3d9d4a2c48d106e5e063e8a40e1bf61a20b1df93
syncvi <pkg417@proton.me>
2024-11-08 16:28:38 +0100

    tf: inteface and output

--- terraform/azure/main_virt_ntwrk.tf
+++ terraform/azure/main_virt_ntwrk.tf
@@ -22,11 +22,13 @@ locals {
     name          = "example-network"
     address_space = ["10.0.0.0/16"]
   }
+  local_interface_name = "demo-iface"
 }
 resource "azurerm_resource_group" "demogrp" {
   name     = local.resource_group_name
   location = local.location
 }
+
 
 resource "azurerm_virtual_network" "demovirnet" {
   name                = local.virt_ntwrk.name
@@ -34,15 +36,39 @@ resource "azurerm_virtual_network" "demovirnet" {
   resource_group_name = local.resource_group_name
   address_space       = local.virt_ntwrk.address_space
 
-  subnet {
-    name             = "subnetA"
-    address_prefixes = ["10.0.0.0/24"]
-  }
-
-  subnet {
-    name             = "subnetB"
-    address_prefixes = ["10.0.1.0/24"]
-  }
-
   depends_on = [azurerm_resource_group.demogrp]
+}
+
+resource "azurerm_subnet" "subnetA" {
+  name                 = "subnetA"
+  resource_group_name  = local.resource_group_name
+  virtual_network_name = azurerm_virtual_network.demovirnet.name
+  address_prefixes     = ["10.0.0.0/24"]
+  depends_on           = [azurerm_virtual_network.demovirnet]
+}
+
+resource "azurerm_subnet" "subnetB" {
+  name                 = "subnetB"
+  resource_group_name  = local.resource_group_name
+  virtual_network_name = azurerm_virtual_network.demovirnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+  depends_on           = [azurerm_virtual_network.demovirnet]
+}
+
+resource "azurerm_network_interface" "demo-interface" {
+  name                = local.local_interface_name
+  location            = local.location
+  resource_group_name = local.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnetA.id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+  depends_on = [azurerm_subnet.subnetA]
+}
+
+output "subnetA-id" {
+  value = azurerm_subnet.subnetA.id
 }
